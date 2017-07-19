17a83c894ff609796f65cf8429377c48e23ae912
m1g <miguelmalcolm@outlook.com>
2017-07-19 00:51:19 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# REACT BOILERPLATE
+
+It's dangerous to go alone, git clone this...
+
+![tri-force image](http://i.imgur.com/BNjZYEH.jpg)
---
+++ app/app.jsx
@@ -0,0 +1,8 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+
+// This allows react to render in the browser. It's attached to the Id 'app' in the index
+  ReactDOM.render(
+    <h1>Boilerplate App!</h1>, // References the Main class
+    document.getElementById('app')
+  );
