608cdaeaaf9f749d8d1af4e2b349783e9477d7ec
m1g <miguelmalcolm@outlook.com>
2017-07-21 15:37:31 -0400

    squished bugs

--- server.js
+++ server.js
@@ -5,25 +5,15 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
 
+app.use(express.static('public'));
+
 app.listen(PORT, function() {
   console.log('Express server is up on port ' + PORT)
 })
-
-// let path = require('path');
-//
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// app.get('/*', function(req, res) {
-//   res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-// });
-//
-// app.listen(3000, function(){
-//   console.log('Express server is up on port 3000');
-// })
